82cbe3fc1992e381dbe1852e62c1b14b213d220a
James Kerr <kerrj@wit.edu>
2020-11-09 12:33:39 -0500

    Event and Database_Facade Interfaces
    added interfaces for event and database_facade for building on

---
+++ Front_End_React/src/Event.js
@@ -0,0 +1,55 @@
+class Event {
+    
+    constructor() {}
+
+    addSession(session) {}
+
+    addTimeslot(timeslot) {}
+
+    addRoom(room) {}
+
+    addSpeaker(speaker) {}
+
+
+    editSession(session) {}
+
+    editTimeslot(timeslot) {}
+
+    editRoom(room) {}
+
+    editSpeaker(speaker) {}
+
+
+    removeSession(sessionId) {}
+
+    removeTimeslot(timeslotId) {}
+
+    removeRoom(roomId) {}
+
+    removeSpeaker(speakerId) {}
+
+
+    retrieveSessions() {}
+
+    retrieveTimeslots() {}
+
+    retrieveRooms() {}
+
+    retrieveSpeakers() {}
+
+
+    retrieveSession(sessionId) {}
+
+    retrieveTimeslot(timeslotId) {}
+
+    retrieveRoom(roomId) {}
+
+    retrieveSpeaker(speakerId) {}
+
+    
+    retrieveSessionsByTimeslot() {}
+
+    retrieveSessionsByRoom() {}
+
+    retrieveSessionsBySpeaker() {}
+}
